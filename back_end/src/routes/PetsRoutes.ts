cffc7e02280f7d799e95c9ae0e9b9568c00291bb
Geovane Silva Prazeres <geovanesil45sil@gmail.com>
2026-05-04 14:29:09 -0300

    projeto

--- back_end/src/routes/PetsRoutes.ts
+++ back_end/src/routes/PetsRoutes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { PetsController } from "../controllers/PetsControllers";
 
+//middlewares
+import { checkToken } from "../helpers/verify-token";
+
 const router = Router();
 
 router.post("/create", PetsController.create);
